15b4b885fd3351036ff73bab2172ec082fda2a93
Fahrur Rifai <mfahrurrifai@gmail.com>
2018-09-19 21:17:40 +0700

    init

---
+++ home.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<title>Beranda</title>
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="fontawesome/css/all.min.css">
+</head>
+<body class="bg-secondary">
+	<div class="container mt-4">
+		<ul class="nav justify-content-center nav-pills nav-fill bg-dark">
+			<li class="nav-item">
+				<a class="nav-link rounded-0 bg-info text-light active" href="#"><i class="fas fa-columns"></i> Beranda</a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link rounded-0 bg-dark text-light" href="#"><i class="fas fa-book"></i> Matakuliah</a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link rounded-0 bg-dark text-light" href="#"><i class="fas fa-clipboard-list"></i> Nilai</a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link rounded-0 bg-dark text-light" href="#"><i class="fas fa-user"></i> Profile</a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link rounded-0 bg-dark text-light" href="#"><i class="fas fa-sign-out-alt"></i> Logout</a>
+			</li>
+		</ul>
+
+		<!-- konten -->
+		<div class="row mt-3">
+			<!-- sidebar -->
+			<div class="col-md-3">
+				<div class="card">
+					<img src="img/bi.jpg" alt="Foto" class="card-img-top">
+					<div class="card-body">
+						<h5 class="card-title">Yoga Dwi J</h5>
+						<p class="card-text">Teknik Informatika</p>
+					</div>
+				</div>
+			</div>
+
+			<!-- konten utama -->
+			<div class="col-md-9">
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptate nisi voluptas maxime ratione, nobis nulla! Architecto unde officiis, voluptate amet. Repellendus eaque impedit reprehenderit culpa animi consectetur distinctio voluptate inventore!
+			</div>
+
+			<!-- footer -->
+			<div class="col-md-12 mt-3">
+				<nav class="navbar navbar-dark bg-dark justify-content-center rounded">
+					<span class="navbar-text">
+						Copyright &copy; Tugas Kita 2018
+					</span>
+				</nav>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
---
+++ login.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en" class="h-100">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<title>Login</title>
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="fontawesome/css/all.min.css">
+</head>
+<body class="h-100 bg-secondary">
+	<div class="container h-100">
+		<div class="row justify-content-center h-100">
+			<div class="col-md-4 align-self-center">
+				
+				<div class="card bg-dark text-light">
+					<div class="card-body">
+
+						<form>
+							<div class="form-group">
+								<label>NIM</label>
+								<input type="text" class="form-control" placeholder="165410011">
+							</div>
+							<div class="form-group">
+								<label>PASSWORD</label>
+								<input type="password" class="form-control" placeholder="******">
+							</div>
+							<div class="form-group text-center mb-1">
+								<button class="btn btn-info"><i class="fas fa-sign-in-alt"></i> LOGIN</button>
+							</div>
+						</form>
+
+					</div>
+				</div>
+
+			</div>
+		</div>
+	</div>
+</body>
+</html>
