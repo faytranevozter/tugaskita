d50864ee9fb63bcf4d6b7f3b7a3ab587ffc7b1b8
Fahrur Rifai <mfahrurrifai@gmail.com>
2018-11-12 17:13:05 +0700

    prevent sql injection

--- config/dosen.class.php
+++ config/dosen.class.php
@@ -3,6 +3,10 @@
 class Dosen extends Database {
 
 	function login($nip, $password) {
+		// prevent sql injection
+		$nip = $this->con->real_escape_string($nip);
+		$password = $this->con->real_escape_string($password);
+
 		$q = $this->con->query("SELECT * FROM dosen WHERE dosen_nip = '{$nip}' AND dosen_password = '{$password}'");
 		if ($q->num_rows > 0) { // jika dosen ditemukan
 			return $q->fetch_assoc(); // kembalikan data login
--- config/mahasiswa.class.php
+++ config/mahasiswa.class.php
@@ -3,6 +3,10 @@
 class Mahasiswa extends Database {
 
 	function login($nim, $password) {
+		// prevent sql injection
+		$nim = $this->con->real_escape_string($nim);
+		$password = $this->con->real_escape_string($password);
+
 		$q = $this->con->query("SELECT * FROM mahasiswa WHERE mahasiswa_nim = '{$nim}' AND mahasiswa_password = '{$password}'");
 		if ($q->num_rows > 0) { // jika mahasiswa ditemukan
 			return $q->fetch_assoc(); // kembalikan data login
